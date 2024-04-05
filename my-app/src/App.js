d91297620c09c88d29b5db7cf3430c6568574844
CodeFizzzieee <badboyboy236@gmail.com>
2024-04-05 21:24:28 +0500

    added App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,58 @@
-
 import './App.css';
 import About from './components/About';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import React,{useState} from 'react';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+  
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
-  const toggleMode =()=>{
-    if(mode==='light'){
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+  };
+
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#344955';
-    }
-    else{
-      setMode('light'); 
+      showAlert('Dark Mode Enabled', 'success');
+      document.title = 'Texter - Dark Mode';
+    } else {
+      setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert('Light Mode Enabled', 'success');
+      document.title = 'Texter - Light Mode';
     }
-  }
+  };
+
   return (
-    <><Navbar mode={mode} toggleMode={toggleMode} />
-    <div className='container my-3'>
-    <TextForm/>
-    <About/>
-    <b/>
-   
-    </div>
+    <>
+      <Router>
+        <Navbar mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className='container my-3'>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          
+          <Route path="/">
+            <TextForm />
+          </Route>
+        </Switch>
+        </div>
+      </Router>
     </>
   );
 }
